357695998c732a2555640fc8436d80381b7ae028
Dilantato <dilanbocanegrac@gmail.com>
2019-06-06 12:13:22 -0500

    Avances de contenedores de productos

---
+++ js/js.js
@@ -0,0 +1,76 @@
+
+
+
+/*------------------------------CAMBIO BURGUER CONTAINER-------------------------------------- */
+
+function cambioBurguerContainer (){
+    document.getElementById("burguer-container").style.display="flex"
+    document.getElementById("burguer-container").style.zIndex=1
+    
+}
+
+function backButton(){
+   
+document.getElementById("burguer-container").style.display="none"
+}
+
+/*------------------------------CAMBIO BURGUER CONTAINER------------------------------------ */
+
+/*-------------------------------------------------------------------- */
+
+/*----------------------------------CAMBIO PATACONES CONTAINER---------------------------------- */
+function cambioPataconesContainer (){
+    document.getElementById("patacones-container").style.display="inherit"
+    document.getElementById("patacones-container").style.zIndex=1
+    
+}
+
+function backButton1(){
+   
+    document.getElementById("patacones-container").style.display="none"
+    }
+/*----------------------------------CAMBIO PATACONES CONTAINER---------------------------------- */
+
+/*-------------------------------------------------------------------- */
+
+/*----------------------------------CAMBIO CARNES CONTAINER---------------------------------- */
+function cambioCarnesContainer (){
+    document.getElementById("carnes-container").style.display="inherit"
+    document.getElementById("carnes-container").style.zIndex=1
+    
+}
+function backButton2(){
+   
+    document.getElementById("carnes-container").style.display="none"
+    }
+/*----------------------------------CAMBIO CARNES CONTAINER---------------------------------- */
+
+/*-------------------------------------------------------------------- */
+
+/*----------------------------------CAMBIO DOGS CONTAINER---------------------------------- */
+function cambioDogsContainer (){
+    document.getElementById("dogs-container").style.display="inherit"
+    document.getElementById("dogs-container").style.zIndex=1
+    
+}
+
+function backButton3(){
+   
+    document.getElementById("dogs-container").style.display="none"
+    }
+/*----------------------------------CAMBIO DOGS CONTAINER---------------------------------- */
+
+/*-------------------------------------------------------------------- */
+
+/*----------------------------------CAMBIO OTROS CONTAINER---------------------------------- */
+function cambioOtrosContainer (){
+    document.getElementById("otros-container").style.display="inherit"
+    document.getElementById("otros-container").style.zIndex=1
+    
+}
+
+function backButton4(){
+   
+    document.getElementById("otros-container").style.display="none"
+    }
+/*----------------------------------CAMBIO OTROS CONTAINER---------------------------------- */
